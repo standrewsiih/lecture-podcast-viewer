258684d387db6330dc2056cf3b82ccdeb067c4d5
Kris Grint <krisgrint@gmail.com>
2017-10-29 18:08:34 +0000

    Make Lecture.js a dumb component

--- src/Lecture.js
+++ src/Lecture.js
@@ -1,19 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import moment from 'moment';
 
-class Lecture extends Component {  
-
-  render() {
-    return (
-      <div className="content-panel lecture">    
-        <h2 className="page-subheading">{this.props.title}</h2>
-        <strong>{moment(this.props.pubDate).format('dddd MMMM Do YYYY')}</strong>
-        <p>{this.props.subtitle}</p>
-        <p><audio preload="none" controls="controls"><source src={this.props.url} type="audio/mpeg" /></audio></p>
-      </div>
-    );
-  }
-
-}
+const Lecture = (props) => {
+  return(
+    <div className="content-panel lecture">    
+      <h2 className="page-subheading">{props.title}</h2>
+      <strong>{moment(props.pubDate).format('dddd MMMM Do YYYY')}</strong>
+      <p>{props.subtitle}</p>
+      <p><audio preload="none" controls="controls"><source src={props.url} type="audio/mpeg" /></audio></p>
+    </div>
+  );
+};
 
 export default Lecture;
